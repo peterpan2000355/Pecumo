ddb86fc0cb8e0d24bfafa128a957bb0523aac89b
Pedro C <83980820+peterpan2000355@users.noreply.github.com>
2023-03-29 04:04:16 +0200

    Add files via upload
        name='ip.py',
        version="1.0",
        description="Calculadora IP mediante el uso de la libreria ipaddress"
        author="Pedro C.",
        license="GPL"

---
+++ ip.py
@@ -0,0 +1,28 @@
+'''(
+    name='ip.py',
+    version="1.0",
+    description="Calculadora IP mediante el uso de la libreria ipaddress"
+    author="Pedro C.",
+    license="GPL"
+)
+'''
+
+import ipaddress
+
+Id=input("Entre la dirección de red IPv4 ó IPv6 con prefijo (ejemplo: 192.168.1.25/31):") 
+ip=ipaddress.ip_network(Id, strict=False)
+print("Mascara de subred:",ip.netmask)
+print("Wildcard es:",ip.hostmask)
+print("Primera IP valida o usable es:",ip.network_address+1)
+print("Última IP valida o usable es:",ip.broadcast_address-1)
+print("Broadcast",ip.broadcast_address)
+if (ip.num_addresses) == 2:   
+    print("Total de direcciones IP validas:", ip.num_addresses)
+else:
+    print("Total de direcciones IP validas:", ip.num_addresses-2)
+print("Total de direcciones IP:", ip.num_addresses)
+print("Es una dirección privada",ip.is_private)
+print("Es una dirección reservada",ip.is_reserved)
+print("Host:",ip.num_addresses)
+
+
