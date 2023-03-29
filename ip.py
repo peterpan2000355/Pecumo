afefb89a2e762006abf2edf8ce2b31be41fd05a2
Pedro C <83980820+peterpan2000355@users.noreply.github.com>
2023-03-29 05:08:45 +0200

    Update ip.py

--- ip.py
+++ ip.py
@@ -10,19 +10,17 @@
 import ipaddress
 
 Id=input("Entre la dirección de red IPv4 ó IPv6 con prefijo (ejemplo: 192.168.1.25/31):") 
-ip=ipaddress.ip_network(Id, strict=False)
+ip=ipaddress.ip_network(Id.strip(), strict=False)  #Eliminamos espacios iniciales y finales, no funciona si metemos un espacio por error
 print("Mascara de subred:",ip.netmask)
 print("Wildcard es:",ip.hostmask)
-print("Primera IP valida o usable es:",ip.network_address+1)
+print("Primera IP valida o usable es:", ip.network_address+1)
 print("Última IP valida o usable es:",ip.broadcast_address-1)
 print("Broadcast",ip.broadcast_address)
-if (ip.num_addresses) == 2:   
+if (ip.num_addresses) == 2:                         # Necesario porque sino hay incoherencia con las IPv4 de mascara 31   
     print("Total de direcciones IP validas:", ip.num_addresses)
 else:
     print("Total de direcciones IP validas:", ip.num_addresses-2)
 print("Total de direcciones IP:", ip.num_addresses)
-print("Es una dirección privada",ip.is_private)
-print("Es una dirección reservada",ip.is_reserved)
-print("Host:",ip.num_addresses)
-
-
+print("Es una dirección privada?:",ip.is_private)
+print("Es una dirección reservada?:",ip.is_reserved)
+print("Es una dirección global?:",ip.is_global)
